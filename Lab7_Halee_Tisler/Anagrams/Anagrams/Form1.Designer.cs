3577fafe3f75daac631c57071eb8840a1c190d19
Halee Tisler <123340415+halee-t@users.noreply.github.com>
2023-04-02 12:13:00 -0400

    Add files via upload

---
+++ Lab7_Halee_Tisler/Anagrams/Anagrams/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Anagrams
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstAnagrams = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(92, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 163);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "List The Anagrams!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstAnagrams
+            // 
+            this.lstAnagrams.FormattingEnabled = true;
+            this.lstAnagrams.ItemHeight = 20;
+            this.lstAnagrams.Location = new System.Drawing.Point(511, 103);
+            this.lstAnagrams.Name = "lstAnagrams";
+            this.lstAnagrams.Size = new System.Drawing.Size(620, 544);
+            this.lstAnagrams.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 743);
+            this.Controls.Add(this.lstAnagrams);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstAnagrams;
+    }
+}
+
---
+++ Lab7_Halee_Tisler/Anagrams/Anagrams/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+/*
+LAB #7 
+FALL SEMESTER
+HALEE, TISLER
+I fully understand the following statement.
+OU PLAGIARISM POLICY
+All members of the academic community at Oakland are expected to practice and uphold ‘standards 
+of academic integrity and honesty. An instructor is expected to inform and instruct ‘students about 
+the procedures and standards of research and documentation required of students ‘in fulfilling 
+course work. A student is expected to follow such instructions and be sure the rules ‘and procedures 
+are understood in order to avoid inadvertent misrepresentation of his work. ‘Students must assume 
+that individual (unaided) work on exams and lab reports and documentation ‘of sources is expected 
+unless the instructor specifically says that is not necessary.
+The following definitions are some examples of academic dishonesty:
+• Plagiarizing from work of others. Plagiarism is using someone else's work or ideas without 
+giving the other person credit; by doing this, a student is, in effect, claiming credit for 
+someone else's thinking. Whether the student has read or heard the information he uses, the 
+student must document the source of information. When dealing with written sources, a 
+clear distinction would be made between quotations (which reproduce information from the 
+source word-for-word within quotation marks) and paraphrases (which digest the source 
+information and produce it in the student's own words). Both direct quotations and 
+paraphrases must be documented. Just because a student rephrases, condenses or selects 
+from another person's work, the ideas are still the other person's, and failure to give credit 
+constitutes misrepresentation of the student's actual work and plagiarism of another's ideas. 
+Naturally, buying a paper and handing it in as one's own work is plagiarism.
+Cheating on lab reports falsifying data or submitting data not based on student's own work.
+*/
+
+namespace Anagrams
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Clear the list
+            lstAnagrams.Items.Clear();
+            
+            //create stream readers for the text files
+            StreamReader DictFile;
+            DictFile = File.OpenText("C:\\Windows\\Temp\\F21\\dict.txt");
+            string DictWord;
+
+            StreamReader PalFile;
+
+            string PalWord;
+
+            //create arrays for the text files
+            int[] dictArray = new int[26];
+            int[] palArray = new int[26];
+
+            //loop that continues until the end of the dict text file
+            while (!DictFile.EndOfStream)
+            {
+                PalFile = File.OpenText("C:\\Windows\\Temp\\F21\\pal.txt");
+
+                DictWord = DictFile.ReadLine();
+
+                //call the Histogram using an array and string for the parameters
+                Histo.Histogram.CreateHistogram(dictArray, DictWord);
+
+                //loop that continues until the end of the pal text file 
+                while (!PalFile.EndOfStream)
+                {
+                    PalWord = PalFile.ReadLine();
+
+                    //call the Histogram
+                    Histo.Histogram.CreateHistogram(palArray, PalWord);
+
+                    int count = 0; //counter for the matches
+
+                    //loop that continues for the length of the pal array
+                    for (int x = 0; x < palArray.Length; x++)
+                    {
+                        //if the anagrams match, increase the count
+                        if (dictArray[x] == palArray[x])
+                        {
+                            count++; //count increases for every match
+                        }
+                    }
+
+                    //if you find all of the matches, list them in the list box
+                    if (count == 26)
+                    {
+                        lstAnagrams.Items.Add(DictWord + " -> " + PalWord);
+                    }
+                }
+                //Close the PalFile
+                PalFile.Close();
+            }
+
+            //Close the DictFile
+            DictFile.Close();
+        }
+    }
+}
